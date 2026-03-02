0b3fe1d41d7ca1c1e3e74f3468babb949c39bd8d
nagymathev <nagymathev@gmail.com>
2026-03-02 10:33:18 +0000

    hella rudimentary authentication

--- server/src/game.rs
+++ server/src/game.rs
@@ -1,10 +1,19 @@
+use std::collections::HashMap;
+
+use axum::http::StatusCode;
+use serde::{Deserialize, Serialize};
+use uuid::Uuid;
+
+#[derive(Deserialize)]
 pub struct Pos {
     x: usize,
     y: usize,
 }
 
-struct Board {
+#[derive(Serialize)]
+pub struct Board {
     players: [char; 2],
+    players_hashed: HashMap<char, Uuid>,
     current_player: usize,
     board: [[char; 3]; 3],
 }
@@ -13,6 +22,7 @@ impl Board {
     pub fn new() -> Board {
         Board {
             players: ['X', 'O'],
+            players_hashed: HashMap::new(),
             current_player: 0,
             board: [
                 ['_','_','_',],
@@ -22,11 +32,59 @@ impl Board {
         }
     }
 
-    fn next_player(mut self: Self) {
+    pub fn add_player(&mut self, id: Uuid) {
+        self.players_hashed.insert(self.players[self.current_player], id);
+    }
+
+    pub fn next_player(&mut self) {
         self.current_player = (self.current_player + 1) % self.players.len();
     }
 
-    fn turn(mut self: Self, pos: Pos) {
-        self.board[pos.y][pos.x] = self.players[self.current_player];
+    pub fn turn(&mut self, id: Uuid, pos: &Pos) -> StatusCode {
+        if self.players_hashed[&self.players[self.current_player]] == id {
+            self.board[pos.y][pos.x] = self.players[self.current_player];
+            return StatusCode::OK
+        }
+        println!("Someone turned without permissions: {id}");
+        StatusCode::FORBIDDEN
+    }
+
+    pub fn check_for_winner(&self) -> Option<char> {
+        let mut winner: Option<char> = None;
+        // Check Rows
+        for y in 0..self.board.len() {
+            if Self::eq_three(self.board[y][0], self.board[y][1], self.board[y][2]) {
+                winner = Some(self.board[y][0]);
+                break;
+            }
+        }
+
+         // Check Columns
+        for x in 0..self.board.len() {
+            if Self::eq_three(self.board[0][x], self.board[1][x], self.board[2][x]) {
+                winner = Some(self.board[0][x]);
+                break;
+            }
+        }
+
+        // Check Diagonals
+        if Self::eq_three(self.board[0][0], self.board[1][1], self.board[2][2]) {
+            winner = Some(self.board[0][0]);
+        }
+        if Self::eq_three(self.board[2][0], self.board[1][1], self.board[0][2]) {
+            winner = Some(self.board[2][0]);
+        }
+
+        winner
+    }
+
+    fn eq_three<T>(a: T, b: T, c: T) -> bool
+    where
+        T: PartialEq
+     {
+        if a == b && b == c && a == c {
+            return true
+        }
+        return false
     }
 }
--- server/src/main.rs
+++ server/src/main.rs
@@ -3,25 +3,31 @@ mod game;
 use std::sync::Arc;
 
 use axum::{
-    Extension, Router, response, extract,
-    routing::{get, post},
+    Extension, Router, extract, http::StatusCode, response, routing::{get, post},
+    Json,
 };
 
+use axum_macros::debug_handler;
 use serde_json::{Value, json};
+use serde::{Serialize, Deserialize};
 use tokio::sync::Mutex;
+use uuid::Uuid;
 
-struct PostData {
-    id: usize,
+#[derive(Deserialize)]
+struct TurnData {
+    id: Uuid,
     pos: game::Pos,
 }
 
 struct State {
-    player_count: usize,
+    board: game::Board,
 }
 
 impl State {
     fn new() -> Self {
-        State { player_count: 0 }
+        State { 
+            board: game::Board::new(),
+        }
     }
 }
 
@@ -33,18 +39,42 @@ async fn main() {
         .route("/", get(|| async { "Hello world!" }))
         .route("/register", get(register))
         .route("/turn", post(turn))
+        .route("/board", get(get_board))
         .layer(Extension(state));
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
 
-async fn register(state: Extension<Arc<Mutex<State>>>) -> response::Json<Value> {
+async fn register(state: Extension<Arc<Mutex<State>>>) -> Json<Value> {
+    let id = Uuid::new_v4();
+    println!("[{}] new registered user: {}", chrono::Local::now(), &id);
+
     let mut state = state.lock().await;
-    state.player_count += 1;
-    let id = state.player_count;
+    state.board.add_player(id);
+    state.board.next_player();
+    Json(json!(
+        {
+            "id": id
+        }
+    ))
+}
+
+async fn health_check(Json(data): Json<Value>) -> StatusCode {
+    StatusCode::OK
+}
+
+#[debug_handler]
+async fn turn(state: Extension<Arc<Mutex<State>>>, Json(pos): Json<TurnData>) -> StatusCode {
+    let mut state = state.lock().await;
+    state.board.turn(pos.id, &pos.pos);
+    state.board.next_player();
+    StatusCode::OK
+}
+
+async fn get_board(state: Extension<Arc<Mutex<State>>>) -> response::Json<Value> {
+    let board = &state.lock().await.board;
     response::Json(json!({
-        "id": id
+        "board": board
     }))
 }
-async fn turn(pos: extract::Json<PostData>) {}
