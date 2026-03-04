74a066629465402d06b2d2d00185c445e05a2fbf
nagymathev <nagymathev@gmail.com>
2026-03-04 12:10:47 +0000

    extra animations on hover

--- client/scripts/board_cell.gd
+++ client/scripts/board_cell.gd
@@ -29,8 +29,21 @@ func set_cell_state(state: BoardData.PlayerStates):
 
 func _ready() -> void:
 	button.pressed.connect(_on_button_pressed)
+	button.mouse_entered.connect(_on_hover)
+	button.mouse_exited.connect(_off_hover)
+	button.pivot_offset_ratio = Vector2(0.5, 0.5)
 
 	set_cell_state(BoardData.PlayerStates.Spectator)
 
 func _on_button_pressed():
 	clicked.emit(board_pos)
+
+func _on_hover():
+	z_index = 10
+	var tween := create_tween()
+	tween.tween_property(button, "scale", Vector2(1.2, 1.2), 0.4).set_trans(Tween.TRANS_BACK).set_ease(Tween.EASE_OUT)
+
+func _off_hover():
+	z_index = 0
+	var tween := create_tween()
+	tween.tween_property(button, "scale", Vector2(1.0, 1.0), 0.4).set_trans(Tween.TRANS_EXPO).set_ease(Tween.EASE_OUT)
